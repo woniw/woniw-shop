a6ba972c1338beb6e9f6201a5b1558801569a187
woniw <woniwwinow@gmail.com>
2025-07-22 21:00:42 -0600

    buy now headphone button got connected with backend servers (flask)

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data.variables import data
 from data.variables import bright_red
 from data.variables import bright_green
 from data.variables import bright_blue 
+from data.variables import bright_yellow
 
 print(f"{bright_blue} LOG: {data}")
 
@@ -10,7 +11,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    balance = data['balance']
+    return render_template("index.html")
+
+@app.route("/dummy_site")
+def dummy_site():
+    return render_template("dummy_site.html")
+
+@app.route('/headphones_buy_now_button', methods=['POST', "GET"])
+def headphones_buy_now_button():
+
+    if request.method == "POST":
+        print(f"{bright_yellow}LOG: working")
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
