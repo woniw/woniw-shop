c5336bcb8730b652c66c4ff6c8a728f7df651fd0
woniw <woniwwinow@gmail.com>
2025-07-23 23:50:01 -0600

    editing test button insufficent_funds.html has been completed new json format

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from data.variables import bright_red
 from data.variables import bright_green
 from data.variables import bright_blue 
 from data.variables import bright_yellow
+from data.variables import bright_magenta
 from data.python_function import save_json
 import logging
 
@@ -14,6 +15,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     balance = data['balance']
+    print("")
+    print('-----------------------------------')
+    print(f"{bright_magenta}BALANCE: {balance}")
+    print('-----------------------------------')
+
     return render_template("index.html", balance=balance) 
 
 @app.route("/cancel_payment")
@@ -26,24 +32,45 @@ def payment_confirmed():
     index_url = url_for("index")
     return render_template("payment_confirmed.html", index_url=index_url)
 
+@app.route('/insufficent_funds')
+def insufficent_funds():
+    index_url = url_for("index")
+    return render_template("insufficent_funds.html", index_url=index_url)
+
 @app.route('/buy_now_page')
 def buy_now_page():
     item_purchase = data['temp_buy_now']['current_Item']
     item_price = data['temp_buy_now']["price"]
     cancel_payment_url = url_for("cancel_payment")
-
-    print(f'{bright_blue}LOG: {item_purchase}')
-    print(f'{bright_blue}LOG: {item_price}')
+    insufficient_funds_message = "insufficient funds"
 
     return render_template("buy_now_page.html", item_purchase=item_purchase, item_price=item_price, cancel_payment_url=cancel_payment_url)
 
 @app.route("/confirm_payment", methods=['POST', 'GET'])
 def confirm_payment():
     if request.method == "POST":
-        return redirect(url_for("payment_confirmed"))
-    else:
-        return redirect(url_for("index"))
+        #! subtracting price
+        balance = data['balance']
+        item_price = data["temp_buy_now"]["price"]
 
+        print(f"{bright_yellow}USER BALANCE: {balance}")
+        print(f"{bright_yellow}ITEM PRICE: {item_price}")
+
+        if balance >= item_price:
+            new_balance = balance - item_price
+            print(f"{bright_red}NEW BALANCE: {new_balance}")
+
+            data["balance"] = new_balance
+            from data.python_function import clear_temp_buy_now
+            clear_temp_buy_now()
+
+            save_json('data.json', data)
+
+            return redirect(url_for("payment_confirmed"))
+        elif item_price > balance:
+            from data.python_function import clear_temp_buy_now
+            clear_temp_buy_now()       
+            return redirect(url_for("insufficent_funds"))
 
 @app.route('/headphones_buy_now_button', methods=['POST', "GET"])
 def headphones_buy_now_button():
@@ -65,6 +92,10 @@ def clear_temp_json():
         from data.python_function import clear_temp_buy_now
         clear_temp_buy_now()
         print(f'{bright_green}CLEARED!')
+
+        data['balance'] == 1000
+        
+        save_json("data.json", data)
         return render_template("index.html")
     else:
         return render_template("index.html")
