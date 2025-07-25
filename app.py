3e5e8a6b4cf131bf3fd4e2ce73cfba1793c1fedb
woniw <woniwwinow@gmail.com>
2025-07-24 22:05:26 -0600

    new character added. many new characters added

--- app.py
+++ app.py
@@ -45,8 +45,12 @@ def buy_now_page():
 @app.route('/about_page')
 def about_page():
     index_url = url_for("index")
-    
     return render_template("about.html", index_url=index_url)
+
+@app.route("/account_page")
+def account_page():
+    index_url = url_for("index")
+    return render_template("account.html", index_url=index_url)
 
 @app.route("/confirm_payment", methods=['POST', 'GET'])
 def confirm_payment():
@@ -74,11 +78,16 @@ def confirm_payment():
             clear_temp_buy_now()       
             return redirect(url_for("insufficent_funds"))
 
+#! REDIRECT PAGE
 
 @app.route('/redirect_to_about_page')
 def redirect_to_about_page():
     print("ABOUT BUTTON DETECTED")
     return redirect(url_for("about_page"))
+
+@app.route('/redirect_to_account_page')
+def redirect_to_account_page():
+    return redirect(url_for("account_page"))
 
 #! ITEM FUNCTIONS
 #? ----------------------------------------------------
@@ -111,7 +120,140 @@ def optimus_buy_now_button():
     else:
         return redirect(url_for("index"))
 
+@app.route('/iman_buy_now_button', methods=['POST', "GET"])
+def iman_buy_now_button():
 
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "Iman"
+        data['temp_buy_now']["price"] = data['items']['Iman']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/gems_buy_now_button', methods=['POST', "GET"])
+def gems_buy_now_button():
+
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "Gems"
+        data['temp_buy_now']["price"] = data['items']['gems']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/mighty_miner_buy_now_button', methods=['POST', "GET"])
+def mighty_miner_buy_now_button():
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "Mighty Miner"
+        data['temp_buy_now']["price"] = data['items']['mighty_miner']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/amin_buy_now_button', methods=['POST', "GET"])
+def amin_buy_now_button():
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "Amin eyvaz"
+        data['temp_buy_now']["price"] = data['items']['amin']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/shiny_amin_buy_now_button', methods=['POST', "GET"])
+def shiny_amin_buy_now_button():
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "Shiny Amin"
+        data['temp_buy_now']["price"] = data['items']['shiny amin']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/amin_eyvaz_buy_now_button', methods=['POST', "GET"])
+def amin_eyvaz_buy_now_button():
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "Amin eyvaz"
+        data['temp_buy_now']["price"] = data['items']['amin']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/mikael_buy_now_button', methods=['POST', "GET"])
+def mikael_buy_now_button():
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "Mikael"
+        data['temp_buy_now']["price"] = data['items']['mikael']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/homad_buy_now_button', methods=['POST', "GET"])
+def homad_buy_now_button():
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "Green Homad"
+        data['temp_buy_now']["price"] = data['items']['homad']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/opp_detector_buy_now_button', methods=['POST', "GET"])
+def opp_detector_buy_now_button():
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "opp detector"
+        data['temp_buy_now']["price"] = data['items']['opp detector']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/shayan_buy_now_button', methods=['POST', "GET"])
+def shayan_buy_now_button():
+    if request.method == "POST":
+        data['temp_buy_now']["current_Item"] = "Shayan"
+        data['temp_buy_now']["price"] = data['items']['shayan']["price"]
+
+        save_json("data.json", data)
+        print(f"{bright_green} json saved!")
+
+        return redirect(url_for("buy_now_page"))
+    else:
+        return redirect(url_for("index"))
+
+
+#! OTHER FUNCTIONS
 @app.route("/reset_balance")
 def reset_balance():
     from data.python_function import save_json
